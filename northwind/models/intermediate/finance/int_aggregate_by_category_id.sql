9e538831f7bd85ae2abf197ba462088f2cbe8091
André Mendes Nunes <branco.de@hotmail.com>
2025-11-18 00:12:28 -0300

    update testes

--- northwind/models/intermediate/finance/int_aggregate_by_category_id.sql
+++
@@ -1,9 +0,0 @@
-with int_aggregate_by_category_id as (
-    select
-        id,
-        count(*)
-    from {{ ref('srg_crm_nova_tabela') }}
-    group by id
-)
-
-select * from int_aggregate_by_category_id
--- northwind/models/staging/crm/srg_crm_nova_tabela.sql
+++
@@ -1,8 +0,0 @@
-with renamed as (
-    select
-        region_id as id,
-        region_description as descricao
-    from {{ ref('raw_crm_nova_tabela') }}
-)
-
-select * from renamed
